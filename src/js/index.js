4c38650886cf4c803e7a42e899a55a0209afaaed
Michał Górny <umpex94@gmail.com>
2022-01-11 20:25:21 +0100

    add function for rules button

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 import '../scss/main.scss';
+
+const buttonRules = document.querySelector('.button-rules');
+const x_Button = document.querySelector('.times-icon');
+
+const rules = document.querySelector('.rules');
+const overlay = document.querySelector('.container__overlay');
+
+const weapons = document.querySelectorAll('.weapon');
+
+x_Button.onclick = () => {
+  rules.classList.remove('active');
+  overlay.classList.remove('active');
+};
+
+buttonRules.onclick = () => {
+  rules.classList.toggle('active');
+  overlay.classList.toggle('active');
+};
+
+console.log('Pokaż mi to w konsoli');
