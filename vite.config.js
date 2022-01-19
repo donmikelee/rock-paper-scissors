d8949e7be72ef9d4ac7a29c2dc07a105b92b3e9d
Michał Górny <umpex94@gmail.com>
2022-01-19 14:15:00 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,4 @@
 import { defineConfig } from "vite";
-import imgUrl from "./images/icon-paper.svg";
-
-document.getElementById("img").src = imgUrl;
-
-console.log(imgUrl);
 
 export default defineConfig({
   base: "/rock-paper-scissors/",
